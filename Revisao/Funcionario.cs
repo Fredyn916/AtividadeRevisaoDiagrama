069de7888e04761851d2734d852ea24deb5e07e2
Fred Marques <fredyn.curso@gmail.com>
2024-07-11 16:13:54 -0300

    Adicionar arquivos de projeto.

---
+++ Revisao/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisao
+{
+    public class Funcionario : Pessoa
+    {
+        public int Matricula { get; set; }
+        public string PlacaVeiculo { get; set; }
+
+        public bool VerificarVeiculo(Automovel automovel)
+        {
+
+        }
+    }
+}
